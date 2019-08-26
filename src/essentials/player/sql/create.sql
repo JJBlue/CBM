1963a652a0efad299f997704f1c0810bcfd46ebf
JJBlue <lieb.julian@gmx.de>
2019-08-26 15:20:45 +0200

    Update create.sql

--- src/essentials/player/sql/create.sql
+++ src/essentials/player/sql/create.sql
@@ -8,3 +8,12 @@ CREATE TABLE IF NOT EXISTS players(
 	
 	PRIMARY KEY (uuid)
 );
+
+CREATE TABLE IF NOT EXISTS friends(
+	friendOne VARCHAR(20) NOT NULL,
+	friendTwo VARCHAR(20) NOT NULL,
+	accepted BOOLEAN,
+	friendstime TIMESTAMP, /* sendRequest or player accepted the request*/
+	
+	PRIMARY KEY (friendOne, friendTwo)
+);
