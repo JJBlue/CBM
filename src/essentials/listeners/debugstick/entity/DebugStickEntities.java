574879003b611fbc9719b1fd7980c896114bd29c
JJBlue <lieb.julian@gmx.de>
2019-08-27 13:17:45 +0200

    Update

---
+++ src/essentials/listeners/debugstick/entity/DebugStickEntities.java
@@ -0,0 +1,19 @@
+package essentials.listeners.debugstick.entity;
+
+import org.bukkit.attribute.Attributable;
+import org.bukkit.entity.Ageable;
+import org.bukkit.entity.Damageable;
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.entity.Sittable;
+import org.bukkit.entity.Tameable;
+
+public class DebugStickEntities {
+	public void idea() {
+		Attributable attributable;
+		LivingEntity livingEntity;
+		Damageable damageable;
+		Ageable ageable;
+		Tameable tameable;
+		Sittable sittable;	
+	}
+}
