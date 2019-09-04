5a9d115705381797b7008c87e601e5a00ca79044
JJBlue <lieb.julian@gmx.de>
2019-09-04 12:01:51 +0200

    Add Warp Commands. But Rest of Warp is missing at this moment

--- src/essentials/warpmanager/warpServer.java
+++ src/essentials/warpmanager/warpServer.java
@@ -1,55 +1,43 @@
 package essentials.warpmanager;
 
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
-//TODO
 public class warpServer {
 	public static boolean onCommand(CommandSender sender, Command cmd, String cmdLabel, String[] args) {
-		if(cmd.getName().equalsIgnoreCase("warp")){
-			if(cmdLabel.equalsIgnoreCase("wa") || cmdLabel.equalsIgnoreCase("warp")){
-				if(args.length == 1){
-					if(args[0].equalsIgnoreCase("help")){
-						sender.sendMessage("/wa list");
-						sender.sendMessage("/wa list <Ordner>");
-						sender.sendMessage("/wa <File> <Ordner>");
-					}else if(args[0].equalsIgnoreCase("list")){
-						
-					}else{
-						
-					}
-				}else if(args.length == 2){
-					if(args[0].equalsIgnoreCase("list")){
-						
-					}else{
-						
-					}
-				}
-			}else if(cmdLabel.equalsIgnoreCase("setwa") || cmdLabel.equalsIgnoreCase("setwarp")){
-				if(sender.hasPermission("setwa.use")){
-					if(args.length == 1){
-						if(args[0].equalsIgnoreCase("help")){
-							sender.sendMessage("/setwa <File> <Ordner>");
-						}else{
-							
-						}
-					}else if(args.length == 2){
-						
-					}
-				}
-			}else if(cmdLabel.equalsIgnoreCase("delwa") || cmdLabel.equalsIgnoreCase("delwarp")){
-				if(sender.hasPermission("delwa.use")){
-					if(args.length == 1){
-						if(args[0].equalsIgnoreCase("help")){
-							sender.sendMessage("/delwa <File> <FileOrdner>");
-						}else{
-							
-						}
-					}else if(args.length == 2){
-						
-					}
-				}
-			}
+		
+		Player p = null;
+		if(sender instanceof Player)
+			p = (Player) sender;
+		
+		switch (args[0].toLowerCase()) {
+			case "warp":
+				
+				if(args.length == 1)
+					WarpManager.openInventory(p);
+				else if(args.length == 2)
+					WarpManager.teleport(p, args[1]);
+				else if(args.length == 3)
+					WarpManager.teleport(Bukkit.getPlayer(args[2]), args[1]);
+				
+				break;
+				
+			case "setwarp":
+				
+				if(args.length < 2 || p == null) break;
+				WarpManager.setWarp(args[1], p.getLocation());
+				
+				break;
+				
+			case "delwarp":
+
+				if(args.length < 2 || p == null) break;
+				WarpManager.setWarp(args[1], p.getLocation());
+				
+				break;
+				
 		}
 		
 		return true;
